2851f0d438de3ef89e01e8f70faeb9020bad65ff
goodness <goodness@student.21-school.ru>
2023-03-09 12:05:38 +0600

    args test

--- server.c
+++ server.c
@@ -1,6 +1,15 @@
 #include "./network.h"
 
-int main() {
+void parse_args(int argc, char *argv[]);
+
+int main(int argc, char *argv[]) {
+    parse_args(argc, argv);
     server_run();
     return 0;
+}
+
+void parse_args(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        printf("%s \n", argv[i]);
+    }
 }
